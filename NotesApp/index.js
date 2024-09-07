078632cff336807f48fd44b6f02bca3877eec029
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-09-07 12:43:52 +0500

    change update storage function

--- NotesApp/index.js
+++ NotesApp/index.js
@@ -6,9 +6,32 @@ createNotes.addEventListener("click", function(){
     // note.innerHTML = `<p contenteditable="true" id="note" class="p-3 my-3 rounded"><i class="bi bi-trash-fill fs-5 deleteBtn"></i></p>`;
     note.innerHTML = `<p contenteditable="true" id="note" class="p-3 my-3 rounded"><img src="images/delete.png" alt="deleteBtn" class="deleteBtn"></p>`;
     notesContainer.appendChild(note);
+    updateStorage();
 
-    let deleteBtn = note.querySelector(".deleteBtn");
-    deleteBtn.addEventListener("click", function(event){
-        notesContainer.removeChild(note);
-    });
+    attachDeleteListener(note.querySelector(".deleteBtn"));
 });
+
+function updateStorage(){
+    localStorage.setItem("Notes", notesContainer.innerHTML);
+}
+
+function showNotes() {
+    let savedNotes = localStorage.getItem("Notes");
+    if (savedNotes) {
+        notesContainer.innerHTML = savedNotes;
+
+        let deleteButtons = document.querySelectorAll(".deleteBtn");
+        deleteButtons.forEach(function (deleteBtn) {
+            attachDeleteListener(deleteBtn);
+        });
+    }
+}
+
+function attachDeleteListener(deleteBtn) {
+    deleteBtn.addEventListener("click", function () {
+        deleteBtn.parentElement.parentElement.remove();
+        updateStorage();
+    });
+}
+
+showNotes();
